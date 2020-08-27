a1a11d9dcf8248dd6904e19c37e44fc9a7e77431
may truong <maytruong>
2020-08-27 16:21:26 +0700

    go home

--- DoAnNet/frmMain.Designer.cs
+++ DoAnNet/frmMain.Designer.cs
@@ -30,21 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ucTreoTuong1 = new DoAnNet.uc.ucTreoTuong();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.panel1.Controls.Add(this.panel9);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.panel6);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.panel5);
@@ -58,41 +71,78 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 500);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // panel2
+            // panel9
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel2.Location = new System.Drawing.Point(200, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(688, 30);
-            this.panel2.TabIndex = 1;
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel9.Location = new System.Drawing.Point(0, 328);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(8, 40);
+            this.panel9.TabIndex = 14;
             // 
-            // panel3
+            // button7
             // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
-            this.panel3.Location = new System.Drawing.Point(0, 52);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(8, 40);
-            this.panel3.TabIndex = 2;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button7.Location = new System.Drawing.Point(3, 328);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(197, 40);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "      Thông tin";
+            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button7.UseVisualStyleBackColor = true;
             // 
-            // panel4
+            // panel8
             // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
-            this.panel4.Location = new System.Drawing.Point(0, 98);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(8, 40);
-            this.panel4.TabIndex = 4;
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel8.Location = new System.Drawing.Point(0, 282);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(8, 40);
+            this.panel8.TabIndex = 12;
             // 
-            // panel5
+            // button6
             // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
-            this.panel5.Location = new System.Drawing.Point(0, 144);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(8, 40);
-            this.panel5.TabIndex = 6;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.Location = new System.Drawing.Point(3, 282);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(197, 40);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "       Hóa đơn";
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel7.Location = new System.Drawing.Point(0, 236);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(8, 40);
+            this.panel7.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.Location = new System.Drawing.Point(3, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(197, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "   Khách hàng";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // panel6
             // 
@@ -109,14 +159,22 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button4.Location = new System.Drawing.Point(3, 190);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(197, 40);
             this.button4.TabIndex = 7;
-            this.button4.Text = "     Treo tường";
+            this.button4.Text = "         Quản lý";
             this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel5.Location = new System.Drawing.Point(0, 144);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(8, 40);
+            this.panel5.TabIndex = 6;
             // 
             // button3
             // 
@@ -125,14 +183,22 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button3.Location = new System.Drawing.Point(0, 144);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(200, 40);
             this.button3.TabIndex = 5;
-            this.button3.Text = "     Treo tường";
+            this.button3.Text = "    In hóa đơn";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel4.Location = new System.Drawing.Point(0, 98);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(8, 40);
+            this.panel4.TabIndex = 4;
             // 
             // button2
             // 
@@ -141,14 +207,22 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button2.Location = new System.Drawing.Point(0, 98);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 40);
             this.button2.TabIndex = 3;
-            this.button2.Text = "   Đeo tay";
+            this.button2.Text = "         Đeo tay";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel3.Location = new System.Drawing.Point(0, 52);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(8, 40);
+            this.panel3.TabIndex = 2;
             // 
             // button1
             // 
@@ -157,20 +231,41 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button1.Location = new System.Drawing.Point(0, 52);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "     Treo tường";
+            this.button1.Text = "    Treo tường";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(688, 30);
+            this.panel2.TabIndex = 1;
+            // 
+            // ucTreoTuong1
+            // 
+            this.ucTreoTuong1.Location = new System.Drawing.Point(200, 30);
+            this.ucTreoTuong1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ucTreoTuong1.Name = "ucTreoTuong1";
+            this.ucTreoTuong1.Size = new System.Drawing.Size(688, 470);
+            this.ucTreoTuong1.TabIndex = 2;
+            this.ucTreoTuong1.Load += new System.EventHandler(this.ucTreoTuong1_Load);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(888, 500);
+            this.Controls.Add(this.ucTreoTuong1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -179,7 +274,6 @@
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đồ án Chuyên đề NET";
-            this.Load += new System.EventHandler(this.frmMain_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -197,6 +291,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button button7;
+        private uc.ucTreoTuong ucTreoTuong1;
     }
 }
 
--- DoAnNet/uc/ucTreoTuong.cs
+++ DoAnNet/uc/ucTreoTuong.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DoAnNet
+namespace DoAnNet.uc
 {
-    public partial class frmMain : Form
+    public partial class ucTreoTuong : UserControl
     {
-        public frmMain()
+        public ucTreoTuong()
         {
             InitializeComponent();
-        }
-
-        private void ucTreoTuong1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
