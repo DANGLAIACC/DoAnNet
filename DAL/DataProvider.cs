1041e66b4ebc7875828a8460b174f356ad3d923a
may21 <may21_tang2@thuvien.ntt.edu.vn>
2020-09-04 16:15:53 +0700

    dang xem task

--- DAL/DataProvider.cs
+++ DAL/DataProvider.cs
@@ -10,7 +10,7 @@ namespace DAL
 {
     public class DataProvider
     {
-        public static SqlConnection KetNoi()
+        public static SqlConnection OpenConnection()
         {
             string strConnection = @"Data Source = (local);Initial Catalog=QL_Dong_Ho;Intergrated Security=True";
             SqlConnection connection = new SqlConnection(strConnection);
@@ -18,35 +18,41 @@ namespace DAL
             return connection;
         }
 
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+        }
         public static DataTable Execute(string query)
         {
+            SqlConnection connection = OpenConnection();
             DataTable table = new DataTable();
             try
             {
-                SqlDataAdapter adapter = new SqlDataAdapter(query, KetNoi());
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
                 adapter.Fill(table);
             }
             catch
             {
                 table = null;
             }
-            KetNoi().Close();
+            CloseConnection(connection);
             return table;
         }
 
         public static bool ExecuteNonQuery(string query)
         {
+            SqlConnection connection = OpenConnection();
             int result;
             try
             {
-                SqlCommand command = new SqlCommand(query, KetNoi());
+                SqlCommand command = new SqlCommand(query, connection);
                 result = command.ExecuteNonQuery();
             }
             catch
             {
                 result = 0;
             }
-            KetNoi().Close();
+            CloseConnection(connection);
             return result > 0;
         }
     }
