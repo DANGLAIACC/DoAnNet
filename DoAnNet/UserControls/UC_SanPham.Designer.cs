d40aa3c41d0d68c684ae875806364ec48e648d1b
may21 <may21_tang2@thuvien.ntt.edu.vn>
2020-09-09 12:56:29 +0700

    temp

---
+++ DoAnNet/UserControls/UC_SanPham.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DoAnNet.UserControls
+{
+    partial class UC_SanPham
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpSanPham = new System.Windows.Forms.FlowLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.flpSanPham.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flpSanPham
+            // 
+            this.flpSanPham.Controls.Add(this.listView1);
+            this.flpSanPham.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpSanPham.Location = new System.Drawing.Point(0, 0);
+            this.flpSanPham.Name = "flpSanPham";
+            this.flpSanPham.Size = new System.Drawing.Size(748, 484);
+            this.flpSanPham.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // UC_SanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flpSanPham);
+            this.Name = "UC_SanPham";
+            this.Size = new System.Drawing.Size(748, 484);
+            this.flpSanPham.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpSanPham;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
