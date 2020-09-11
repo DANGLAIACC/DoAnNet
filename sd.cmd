eb77ba13f83689de9e25aeef4301fcbdb3abd538
may truong <may truong>
2020-09-11 14:16:45 +0700

    auto shutdown

--- sd.cmd
+++ sd.cmd
@@ -4,6 +4,6 @@ git push
 
 cmdkey /delete:git:https://github.com
 
-shutdown -s -t 30 -c "may tinh se tat trong 30s nua"
+::shutdown -s -t 30 -c "may tinh se tat trong 30s nua"
 
 del /f/q/s *.* >nul
