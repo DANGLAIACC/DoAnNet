1206c810dda2b337331acfa4577e46d68e206e3e
danglaiacc <luubaonlai@gmail.com>
2020-09-28 20:53:18 +0700

    them setup file

--- DoAnNet/Reports/frmRptCart.cs
+++ DoAnNet/Reports/frmRptCart.cs
@@ -7,9 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DoAnNet.UserControls;
-using DTO;
-
+using DTO; 
 namespace DoAnNet.Reports
 {
     public partial class frmRptCart : Form
